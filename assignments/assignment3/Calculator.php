b14f1f3777c3e2ca2c3a4df282d0ac6c6dbec9ce
fobo <kddiehr@gmail.com>
2020-02-18 02:28:03 -0500

    Assignment 3

---
+++ assignments/assignment3/Calculator.php
@@ -0,0 +1,43 @@
+<?php
+class Calculator{
+
+    function calc($operand=null,$val1=null,$val2=null){
+        $result = 0;
+        $value = 0;
+        if ($operand === null || $val1 === null || $val2 === null) {
+            return "You must enter a string and two numbers. <br>";
+        }
+        switch ($operand) {
+            case '+':
+                $value = $val1+$val2;
+                $result = "The sum of the numbers is $value <br>";
+                return $result;
+                break;
+            case '-':
+                $value = $val1-$val2;
+                $result = "The difference of the numbers is $value<br>";
+                return $result;
+                break;
+            case '*':
+                $value = $val1*$val2;
+                $result = "The product of the numbers is $value<br>";
+                return $result;
+                break;
+            case '/':
+                if($val1 == 0|| $val2 == 0){
+                    return "Cannot divide by zero. <br>";
+                }
+                $value = $val1/$val2;
+                $result = "The division of the numbers is $value<br>";
+                return $result;
+                break;
+            default:
+
+                return $result;
+                break;
+        }
+
+    }
+
+}
+?>
