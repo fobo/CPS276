475878a40994c56578f71f4c11864ace21e01072
fobo <kddiehr@gmail.com>
2020-02-20 13:26:12 -0500

    Added comments and one update to default case for operand checking

--- assignments/assignment3/Calculator.php
+++ assignments/assignment3/Calculator.php
@@ -1,10 +1,10 @@
 <?php
 class Calculator{
 
-    function calc($operand=null,$val1="NaN",$val2="NaN"){
+    function calc($operand=null,$val1="NaN",$val2="NaN"){ //Sets default values, so I don't have to use any magic methods
         $result = 0;
         $value = 0;
-        if ($operand === null || !is_int($val1) || !is_int($val2)) {
+        if ($operand === null || !is_int($val1) || !is_int($val2)) { // Checks if the user put in the right things
             return "You must enter a string and two numbers. <br>";
         }
         switch ($operand) {
@@ -31,8 +31,8 @@ class Calculator{
                 $result = "The division of the numbers is $value<br>";
                 return $result;
                 break;
-            default:
-
+            default:  //This case should only happen if the user inputs a non-accepted operand.
+                return "You must enter a string and two numbers. <br>";
                 return $result;
                 break;
         }
